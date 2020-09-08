92341e463bc500dec2d2d4d4b28dc809c95645d7
migliom <54515622+migliom@users.noreply.github.com>
2020-09-08 11:13:23 -0500

    Add files via upload

--- Twitterapi.py
+++ Twitterapi.py
@@ -26,12 +26,11 @@ def buildTrainingSet(corpusFile, tweetFile, twitter_api):
 		readLines  = csv.reader(csvfile,delimiter=',', quotechar="\"")
 		for row in readLines:
 			corpus.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
-#-------------------------------------------------------------------------------#
-	refresh_rate = 180
+
+	refresh_rate = 180 #Time to comply with Twitter's pull policy
 	sleep_timer = 900 / 180
 
 	trainingSet = []
-	x = 1
 	for tweet in corpus:
 		try:
 			print(tweet)
@@ -39,12 +38,9 @@ def buildTrainingSet(corpusFile, tweetFile, twitter_api):
 			tweet["text"] = status.text
 			trainingSet.append(tweet)
 			time.sleep(sleep_timer)
-			x +=1
-			if x > 10:
-				break
 		except:
 			continue
-#-------------------------------------------------------------------------------#
+
 	with open(tweetFile,'wt') as csvfile:
 		writeLines = csv.writer(csvfile,delimiter=',',quotechar="\"")
 		for tweet in trainingSet:
@@ -53,23 +49,17 @@ def buildTrainingSet(corpusFile, tweetFile, twitter_api):
 			except Exception as e:
 				print(e)
 	return trainingSet
-#-------------------------------------------------------------------------------#
+
 import re
 from nltk.tokenize import word_tokenize
 from string import punctuation
 from nltk.corpus import stopwords
 
 class tweetCleanup:
-	def __init__(self):
+	def __init__(self): #DEFINE STOP WORDS
 		self.stopwords = set(stopwords.words('english') + list(punctuation) + ['AT_USER','URL'])
 
-	def processTweets(self, list_of_tweets):
-		processedTweets=[]
-		for tweet in list_of_tweets:
-			processedTweets.append((self.cleanTweet(tweet["text"]),tweet["label"]))
-		return processedTweets
-
-	def cleanTweet(self, tweet):
+	def cleanTweet(self, tweet): #REGEX CLEANUP OF THE TWEET
 		tweet = tweet.lower() # convert text to lower-case
 		tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet) # remove URLs
 		tweet = re.sub('@[^\s]+', 'AT_USER', tweet) # remove usernames
@@ -77,7 +67,13 @@ class tweetCleanup:
 		tweet = word_tokenize(tweet) # remove repeated characters (helloooooooo into hello)
 		return [word for word in tweet if word not in self.stopwords]
 
-#-------------------------------------------------------------------------------#
+	def processTweets(self, list_of_tweets):
+		processedTweets=[]
+		for tweet in list_of_tweets:
+			processedTweets.append((self.cleanTweet(tweet["text"]),tweet["label"]))
+		return processedTweets
+
+
 import nltk
 
 def find_nGrams(preprocessedTrainingData):
@@ -90,8 +86,6 @@ def find_nGrams(preprocessedTrainingData):
     word_features = wordlist.keys()
 
     return word_features
-
-# ------------------------------------------------------------------------
 
 def extract_features(tweet):
     tweet_words=set(tweet)
@@ -110,11 +104,7 @@ if __name__ == "__main__":
 	word_features = find_nGrams(preprocessedTrainingSet)
 	trainingFeatures=nltk.classify.apply_features(extract_features,preprocessedTrainingSet)
 
-	# ------------------------------------------------------------------------
-
 	NBayesClassifier=nltk.NaiveBayesClassifier.train(trainingFeatures)
-
-	# ------------------------------------------------------------------------
 
 	NBResultLabels = [NBayesClassifier.classify(extract_features(tweet[0])) for tweet in preprocessedTestSet]
 
